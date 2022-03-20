8aff7590fe5db0ca2ea081557fe73d143d9851ca
Justin Merwin <justinrm@users.noreply.github.com>
2022-03-19 18:45:21 -0700

    Fibonacci Sequence Generator
    Fibonacci sequence generation using recursion. 
    Currently, any numbers around 50 or larger will take a long time to return. I don't know how to fix this issue yet. Is it a time complexity issue? Do I need to create a cache?

---
+++ fib.py
@@ -0,0 +1,30 @@
+# Author: Justin Merwin
+# Program: fib.py
+#
+# Purpose:
+#
+# Recursively find n terms in the Fibonacci sequence.
+# I know this has been done many times, but this is my take on it.
+#
+# ISSUES:
+#   - Currently, larger numbers (around 50) make the program freeze up on computers with less power.
+#     I don't know how to make it work any better. Perhaps using a cache of sorts?
+
+def fib(n):
+    if n <= 1:
+        return n
+    else:
+        return fib(n-1) + fib(n-2)
+
+num_terms = int(input("How many terms deep would you like displayed? \n >>> "))
+
+while True:
+    terms = []
+    if num_terms <= 0:
+        print("Please enter a positive integer.")
+        break
+    else:
+        for i in range(num_terms):
+            terms.append(fib(i))
+        print(*terms, sep=' - ')
+        break
