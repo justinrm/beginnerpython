bf323108c9e7e344f75b4d5cf827d51303fc5de8
Justin Merwin <justinrm@users.noreply.github.com>
2022-03-24 19:36:12 -0700

    Rock, Paper, Scissors
    Removed unnecessary code.

--- rps.py
+++ rps.py
@@ -13,8 +13,8 @@ losses = 0
 i = 0
 
 def userWin():
-    print(f"{user_name} chose " + choices.get(user_choice) + " and the computer chose " + choices.get(npc_choice) + f"! {user_name} won!")
-    keepScore()
+           print(f"{user_name} chose " + choices.get(user_choice) + " and the computer chose " + choices.get(npc_choice) + f"! {user_name} won!")
+           keepScore()
 
 def keepScore():
     print(f"{user_name}, you have {wins} win(s), {ties} tie(s), and {losses} loss(es).")
@@ -23,29 +23,22 @@ for i in range(rounds):
     user_choice = int(input(f"What is your choice, {user_name}? \n 1: Rock \n 2: Paper \n 3: Scissors \n >>> "))
     npc_choice = random.randint(1,3)
     if user_choice == npc_choice:
-        i += 1
         ties += 1
         print(f"{user_name}, you tied with the computer. You both chose " + choices.get(user_choice) + ".")
         keepScore()
     elif user_choice == 1 and npc_choice == 3:
-        i += 1
         wins += 1
         userWin()
     elif user_choice == 2 and npc_choice == 1:
-        i += 1
         wins += 1
         userWin()
     elif user_choice == 3 and npc_choice == 2:
-        i += 1
         wins += 1
         userWin()
     else:
-        i += 1
         losses += 1
         print(f"Uh oh! {user_name} chose " + choices.get(user_choice) + " and the computer chose " + choices.get(npc_choice) + f"... {user_name} lost!")
         keepScore()
 
-print(f"The game has ended after {rounds}.")
+print(f"The game has ended after {rounds} rounds.")
 keepScore()
-
- 
